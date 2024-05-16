bf950eb989804c08c81c5df520ce90ac3395f129
Supriya Patidar <75739890+supriya-045@users.noreply.github.com>
2024-05-16 08:31:00 +0530

    fix cors issue

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,17 @@
 import express from "express";
 import storageRouter from "./routes/storeRoutes.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: false }));
+
+app.use(cors({
+    origin: "*",
+    methods: "GET,POST,PUT,PATCH",
+    credentials: true
+  }));
 
 app.get("/", (req, res) => {
     return res.status(200).send({
